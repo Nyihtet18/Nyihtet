354b85a57a12e62fc05180f9297caaf750a4f0bb
Nyihtet18 <86526867+Nyihtet18@users.noreply.github.com>
2021-06-27 16:43:45 +0630

    Add files via upload

---
+++ bot.sh
@@ -0,0 +1,188 @@
+#!bin/bash
+pkg update
+apt update
+apt upgrade
+pkg upgrade
+pkg install php
+pkg install nano
+pkg install figlet
+pkg install toilet
+pkg install ruby
+gem install lolcat
+pkg install python
+pkg install python2
+pkg install bash
+pkg install git
+pkg install wget
+pkg install jq
+pkg install pip
+pip install requests module bs4
+apt install git -y
+pkg install python
+pkg install python2-y
+pip install requests
+pip2 install mechanize
+pip install lolcat
+pkg install -upgrade pip
+pkg install asyncio
+pip install coloroma
+pip install telethon
+pip install bs4
+pip install wheel
+pip install async_generator
+apt-get install tor
+apt install git
+apt-get install privoxy
+pip2 install requests
+pip3 install requests
+pkg install tesseract
+pkg install imagemagick
+clear
+figlet Install |lolcat
+figlet Success |lolcat
+echo Rescan Script Please Wait A Few Minutes |lolcat
+echo "***   *** ***    *** ***" | lolcat
+figlet  N | lolcat
+echo "***   *** ****   *** ***" | lolcat
+figlet Y | lolcat
+echo "***   *** *****  *** ***" | lolcat
+figlet I | lolcat
+echo "********* ********** ***" | lolcat
+echo Install Information Wait...... | lolcat
+echo "1 Minutes To 5 Minutes Success Congratulations " | lolcat
+figlet H | lolcat
+echo "********* ********** ***" | lolcat
+figlet T | lolcat
+echo "***   *** *** ****** ***" | lolcat
+figlet E | lolcat
+echo "***   *** ***   **** ********" | lolcat
+figlet T | lolcat
+echo "***   *** ***    *** ********" | lolcat
+figlet Wait... | lolcat
+echo Install Information Wait...... | lolcat
+clear
+pkg update
+apt update
+apt upgrade
+pkg upgrade
+figlet Wait...|lolcat
+pkg install php
+pkg install nano
+figlet Wait |lolcat
+pkg install figlet
+pkg install toilet
+figlet Wait... .... ...        ......        ..... |lolcat
+pkg install ruby
+gem install lolcat
+clear
+echo Wait......      Wait.......     ****** *****       ************                     ********      Wait.... | lolcat
+pkg install python
+pkg install python2
+pkg install bash
+pkg install git
+figlet Wait...... | lolcat
+pkg install wget
+pkg install jq
+pkg install pip
+pkg install --upgrade pip
+figlet Pip  | lolcat
+echo Install Success |lolcat
+figlet Wait....... | lolcat
+pip install requests module bs4
+apt install git -y
+figlet Wait....... | lolcat
+pkg install python
+pkg install python2-y
+echo Python | lolcat
+figlet Install | lolcat
+figlet Success | lolcat
+figlet Wait....... | lolcat
+pip install requests
+pip2 install mechanize
+pip install lolcat
+figlet Wait..... | lolcat
+pkg install -upgrade pip
+figlet Wait...... | lolcat
+echo Need Required Install Wait........ | lolcat
+pkg install asyncio
+pip install coloroma
+pip install telethon
+pip install bs4
+pip install wheel
+pip install async_generator
+apt-get install tor
+apt install git
+echo Install Success  | lolcat
+figlet Wait..... | lolcat
+figlet Installation | lolcat
+apt-get install privoxy
+pip2 install requests
+pip3 install requests
+pkg install tesseract
+echo Install......... | lolcat
+pkg install imagemagick
+echo Please Permission File | lolcat
+figlet Wait.......... | lolcat
+
+termux-setup-storage
+clear
+
+
+
+
+
+clear
+figlet Install |lolcat
+echo Success!! |lolcat
+figlet Nyihtet |lolcat
+figlet Rewritten |lolcat
+echo "N®®®®®    √√√√√√√         √√ √√√√√ √√     √√" | lolcat    
+echo "N®®®®®    √√√√              √√√√√ √√√√√ √√√     √" | lolcat    
+echo "N®®®®®    √√√√√             √√√√ √√√√√ √     √√√" | lolcat    
+echo "©©©©                       ©©©                               ©©                          ©                 √ ©©©√"|lolcat
+
+echo "©©©©                       ©©©                               ©©                          ©                 √ ©©©√"|lolcat
+
+
+echo "©©©©                       ©©©                               ©©                          ©                 √ ©©©√"|lolcat
+
+clear
+
+figlet "Install" |lolcat
+echo "Success *****.             ******              *****Script"|lolcat
+figlet "N" | lolcat
+figlet Nyihtet | lolcat
+echo Thanks | lolcat
+figlet You |lolcat
+echo  "Lucky my friends!subscribe me!" | lolcat
+figlet S | lolcat
+echo "ubscribe Me Channel Group All Friends Thanks You For Your Support Me" | lolcat
+echo Review Scan!!!!!!!!! |lolcat
+pkg update
+pkg upgrade
+
+
+
+          
+   echo "Wait.....         Wait......            Wait..........            Wait.......     Wait.....         Wait......            Wait..........            Wait.......     Wait.....         Wait......            Wait..........            Wait.......     Wait.....         Wait......            Wait..........            Wait.......     Wait.....         Wait......            Wait..........            Wait.......     Wait.....         Wait......            Wait..........            Wait.......     Wait.....         Wait......            Wait..........            Wait.......     Wait.....         Wait......            Wait..........            Wait.......     Wait.....         Wait......            Wait..........            Wait.......     Wait.....         Wait......            Wait..........            Wait.......     Wait.....         Wait......            Wait..........            Wait.......     Wait.....         Wait......            Wait..........            Wait.......     Wait.....         Wait......            Wait..........            Wait.......     Wait.....         Wait......            Wait..........            Wait.......     Wait.....         Wait......            Wait..........            Wait.......     Wait.....         Wait......            Wait..........            Wait.......     Wait.....         Wait......            Wait..........            Wait.......     Wait.....         Wait......            Wait..........            Wait.......     "|lolcat
+   echo Congratulations ,Your install success!! | lolcat
+figlet Thanks | lolcat 
+              figlet You | lolcat  
+figlet Nice! | lolcat
+figlet Nyihtet...... | lolcat
+                                                                                                                                      echo  Nyihtet...Say You Install Success!! |lolcat
+echo Channel Group number.1[https://www.facebook.com/groups/377866699901108/?ref=share] | lolcat
+ echo Channel Group number.2[https://www.facebook.com/groups/486245642274304/?ref=share] | lolcat
+
+
+
+
+
+
+
+
+
+
+
+                  figlet "N" |lolcat
+                         
